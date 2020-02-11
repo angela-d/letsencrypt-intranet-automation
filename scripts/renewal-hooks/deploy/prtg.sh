ab7b75fe43ef2fc806dbfe93261bb46e5ebefc81
angela <33108490+angela-d@users.noreply.github.com>
2020-02-11 17:24:10 -0600

    Add fullchain.pem for Grafana to PRTG script
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

--- scripts/renewal-hooks/deploy/prtg.sh
+++ scripts/renewal-hooks/deploy/prtg.sh
@@ -5,5 +5,8 @@
 \cp /home/syncthing/example.com/chain.pem /home/syncthing/cert_prtg/root.pem && unix2dos /home/syncthing/cert_prtg/root.pem
 \cp /home/syncthing/example.com/privkey.pem /home/syncthing/cert_prtg/prtg.key && unix2dos /home/syncthing/cert_prtg/prtg.key
 
+# prtg doesn't seem to require fullchain, but if you use grafana, it will
+\cp /home/syncthing/example.com/privkey.pem /home/syncthing/cert_prtg/fullchain.pem && unix2dos /home/syncthing/cert_prtg/fullchain.pem
+
 # send confirmation message to notification chat room
 curl -X POST -H 'Content-Type: application/json' -d '{"text": "PRTG cert prepared."}' 'https://YOUR_CHAT_API/v2/YOUR_TOKEN'
